cba8876c8ecdfd5f4a2a8eab4f121e889bb9d470
KA878 <karim.a.aziz@gmail.com>
2019-02-25 14:22:12 +0100

    update1

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+      
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <h1>Welcome to Codaisseur</h1>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
